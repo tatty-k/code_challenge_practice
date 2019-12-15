f7c4bc21fc10362e0980b8be2ffe4ac604182813
Tatty <tattyklass@gmail.com>
2019-12-14 17:51:25 -0800

    add intersect and right-rotate

--- challenge_practice.js
+++ challenge_practice.js
@@ -18,3 +18,33 @@ var removeDuplicates = function (nums) {
     }
   return nums
   };
+
+  //need to determine small vs big array and remove matching elements from larger array
+  var intersect = function(nums1, nums2) {
+    const intersections = []
+    const small = nums1.length < nums2.length ? nums1 : nums2
+    const big = nums1 === small ? nums2 : nums1
+    for(let i = 0; i < small.length; i++){
+        let idx = big.indexOf(small[i]);
+            if(small[i] === big[idx]){
+                intersections.push(big.splice(idx,1))
+        }
+    }  
+    return intersections
+};
+
+//calculate index of numbers using modulus 
+//loop through list and place numbers at new indexes
+var rotate = function(nums, k) {
+  let moves = nums.length - k % nums.length;
+  let rotated = [...nums]
+  
+  for(let i=0; i < nums.length; i++){
+      rotated.splice((i - moves),1,nums[i])
+  }
+  
+  for(let i=0; i< nums.length; i++){
+  nums[i] = rotated[i] 
+} 
+  return nums
+};
