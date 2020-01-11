c988c91eb8792d3cd7d43d5ddb37b053a45d3d80
Tatty <tattyklass@gmail.com>
2020-01-10 19:12:31 -0800

    add myAtoi, strStr and merge

--- challenge_practice.py
+++ challenge_practice.py
@@ -38,3 +38,76 @@ def reverse_string(s):
                 s[last] = stored
                 first = first + 1
                 last = last - 1
+
+# String to Integer
+# Builds temporary string of digits by looping through original string and adding digits to tmp. Converts tmp to int, checks that it is in range and returns value. 
+def myAtoi(str):
+    
+    # remove zeros
+    str = str.strip()  
+    
+    # initiate variables
+    tmp = "0"
+    i = 0
+    result = 0
+    max_int = 2147483647
+    min_int = -2147483648
+    
+    # checks to make sure string isn't empty - otherwise next condition won't work
+    if len(str) == 0:
+        return 0
+    
+    #if first character in string is + or -: 
+    #re-assign tmp to + or - 
+    #re-assign index to 1, otherwise, loop below will never start (-/+ are not digits)   
+    if str[0] in '+-':
+        tmp = str[0]
+        i = 1
+        
+    # loop through string, add digit characters to temp
+    for i in range(i, len(str)):
+        if str[i].isdigit():
+            tmp += str[i]
+        else:
+            # break if non-digit is found - solves "3.33" (breaks at ".") and "lkjl567" (breaks immediatly and returns 0)
+            break
+    print(tmp)
+    if len(tmp) > 1:
+        result = int(tmp)
+    if result > max_int > 0:
+        return max_int
+    elif result < min_int < 0:
+        return min_int
+    else:
+        return result
+
+#Implement strStr()
+#Need to look into KMP algorithm to improve runtime
+def strStr():
+    # return 0 if string is empty
+        if needle == "":
+            return 0
+        
+        for i in range(len(haystack)-len(needle)+1):
+            for j in range(len(needle)):
+                # if first letter of haystack dosen't match first letter of needle, break inner loop, go to next index in outer loop, and compare the second letter of haystack with the first letter of needle. 
+                # haystack[i+j]: outer index is a marker for the current starting point being checked in the haystack string. j is added to i check subequent characters in haystack so they can be compaired to subsequent letters in needle 
+                # inner loop is broken if a letter from needle does not match current section of haystack
+                if haystack[i+j] != needle[j]:
+                    break
+                # if we reach the end of the needle string and all letters in current section of haystack match the needle string, return outer index
+                if j == len(needle)-1:
+                    return i
+        return -1
+
+#need to re-visit and figure out how to do without sort function
+def merge(num1, num2):
+    idx = len(nums1) - 1
+        
+        for i in range(len(nums2)):
+            if nums1[idx] == 0:
+                nums1[idx] = nums2[i]
+                idx -= 1
+        
+        nums1.sort()
+
